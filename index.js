ff4d88cdec79ca99b17399cf09536a38162810b8
dvTinh99 <dvtinh.it@gmail.com>
2024-04-03 21:03:09 +0700

    check exist

--- index.js
+++ index.js
@@ -8,29 +8,29 @@ console.log('process.env.API_KEY', process.env.API_KEY);
 const API_KEY = process.env.API_KEY || 'dTMgs7iTSPx9TqtJr83jtm24PeRizv6Yr8N3IjIvGsN6qnyPzApmjB4RlLkqW4Ba';
 const SECRET_KEY = process.env.SECRET_KEY || 'zPHxZEGLmV4bHJuEvDv61Ie26nndINHP5OxLcM9M5vcFbdLBoo053muhnCPl9k1G';
 const binance = new ccxt.binance({
-    apiKey : process.env.API_KEY,
-    secret : process.env.SECRET_KEY,
+    apiKey : API_KEY,
+    secret : SECRET_KEY,
 });
 
 var buyed = false;
 var initOpen = 0;
 var totalBuyed = 0;
 const COIN_TRADE =  'W/USDT';
-const USDT_TRADE = 25;
-const MAX = 1.1;
+const USDT_TRADE = 30;
+const MAX = 2;
 
 var count = 0;
 
-binance.setSandboxMode(true);
+// binance.setSandboxMode(true);
 
 async function printBalance() {
     
     const balance = await binance.fetchBalance();
     const coins = balance['info']['balances'];
     const USDT = coins.find( e => e.asset == 'USDT');
-    const ALGO = coins.find( e => e.asset == 'ALGO');
+    const W = coins.find( e => e.asset == 'W');
     console.log('USDT', USDT);
-    console.log('ALGO', ALGO);
+    console.log('W', W);
     
 }
 
@@ -38,60 +38,63 @@ async function getBalance(coin) {
     
     const balance = await binance.fetchBalance();
     const coins = balance['info']['balances'];
-    const numberInBalance = coins.find( e => e.asset == 'coin');
+    const numberInBalance = coins.find( e => e.asset == coin);
     console.log('numberInBalance', numberInBalance);
+    return numberInBalance.free;
     
 }
 async function tick() {
     try {
         count += 1;
-        const prices = await binance.fetchOHLCV(COIN_TRADE, '1s', undefined, 1);
-        let open = prices[0][1]
-        let close = prices[0][4]
-        let height = prices[0][2]
-
+        const load = await binance.fetchTicker(COIN_TRADE);
+        
+        if (load.open > 0) {
+            const prices = await binance.fetchOHLCV(COIN_TRADE, '1s', undefined, 1);
+            let open = prices[0][1]
+            let close = prices[0][4]
+            let height = prices[0][2]
+            if (!buyed) {
+                console.log({
+                    open : open,
+                    close : close,
+                    height : height,
+                });
+                initOpen = open;
+                
+                if (open < MAX) {
+                    buyed = true;
+    
+                    totalBuyed = USDT_TRADE / open;
         
         
-        if (!buyed) {
-            console.log({
-                open : open,
-                close : close,
-                height : height,
-            });
-            buyed = true;
-            initOpen = open;
-
-            totalBuyed = USDT_TRADE / open;
-
-
-            const order = await binance.createLimitBuyOrder(COIN_TRADE, totalBuyed, open)
-            console.log('tốn usdt :', order.cost);
-            console.log('đổi được :', order.amount);
-            console.log('ở giá:', order.price);
-
-            printBalance();
-
-            // console.log({
-            //     open : open,
-            //     close : close,
-            //     height : height,
-            // });
-            // const orderSell = await binance.createLimitSellOrder(COIN_TRADE, totalBuyed, close);
-            // console.log('đã bán :', orderSell.amount);
-            // console.log('ở giá :', orderSell.price);
-
-            // printBalance();
-
-        }
-        if (height == initOpen * 3) {
-            console.log({
-                open : open,
-                close : close,
-                height : height,
-            });
-            const order = await binance.createLimitSellOrder(COIN_TRADE, totalBuyed, open);
-            console.log('đã bán :', order.amount);
-            console.log('ở giá :', order.price);
+                    // const order = await binance.createLimitBuyOrder(COIN_TRADE, totalBuyed, open + 0.05)
+                    console.log('tốn usdt :', order.cost);
+                    console.log('mua được :', order.amount);
+                    console.log('ở giá:', order.price);
+                }
+    
+                printBalance();
+    
+            }
+            if (height / initOpen > 5) {
+                console.log({
+                    open : open,
+                    close : close,
+                    height : height,
+                });
+                // const order = await binance.createLimitSellOrder(COIN_TRADE, totalBuyed, open);
+                console.log('đã bán x5:', order.amount);
+                console.log('ở giá :', order.price);
+            } else if(height / initOpen > 3) {
+                console.log({
+                    open : open,
+                    close : close,
+                    height : height,
+                });
+                // const order = await binance.createLimitSellOrder(COIN_TRADE, totalBuyed, open);
+                console.log('đã bán x3:', order.amount);
+                console.log('ở giá :', order.price);
+            }
         }
     } catch (error) {
         console.log('error catch ne' + count, error);
@@ -101,7 +104,7 @@ async function tick() {
 async function main() {
     
     // tick();
-    setInterval(tick, 500);
+    setInterval(tick, 100);
     
 
 }
@@ -120,5 +123,13 @@ async function buySpotLimit(coin, cost) {
 }
 
 main()
+async function checkCoin(coin) {
+    const fetchStatus = await binance.fetchTicker(coin);
+    console.log('fetchStatus', fetchStatus);
+    
+}
+
+// checkCoin('W/USDT');
 // printBalance();
+// console.log('getBalance("BNB")', await getBalance('BNB'));
 // buySpotMarket('ALGO/USDT', 100)
